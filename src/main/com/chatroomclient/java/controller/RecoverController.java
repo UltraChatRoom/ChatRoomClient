66ac3bf89f43644e4c1fbc69b11b37ea639c63c8
GirtSeanking <2938922672@qq.com>
2020-11-18 10:56:54 +0800

    修改注册界面按钮

---
+++ src/main/com/chatroomclient/java/controller/RecoverController.java
@@ -0,0 +1,10 @@
+package main.com.chatroomclient.java.controller;/**
+* @className: RecoverController
+* @program: ChatRoomClient
+* @description: //TODO 找回密码控制器
+* @author: GirtSeanking
+* @create: 2020-11-18 10:26
+**/
+
+public class RecoverController {
+}
